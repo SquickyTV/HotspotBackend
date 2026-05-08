346d82e88a606b1830673a338b124322062023d6
Squicky <123>
2026-05-07 20:57:58 -0600

    fix port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const cors = require("cors");
 const app = express();
 
-app.use(cors({ origin: "https://hotspotman.netlify.app" }));
+app.use(cors({ origin: "*" }));
 
 let computer;
 
@@ -12,15 +12,20 @@ app.get("/computer", async (req, res) => {
     res.send(computer);
     return;
   }
-  const resp = await axios.post(
-    "https://engine.hyperbeam.com/v0/vm",
-    {},
-    {
-      headers: { Authorization: `Bearer ${process.env.HB_API_KEY}` },
-    }
-  );
-  computer = resp.data;
-  res.send(computer);
+  try {
+    const resp = await axios.post(
+      "https://engine.hyperbeam.com/v0/vm",
+      {},
+      {
+        headers: { Authorization: `Bearer ${process.env.HB_API_KEY}` },
+      }
+    );
+    computer = resp.data;
+    res.send(computer);
+  } catch (err) {
+    console.error("Hyperbeam error:", err.message);
+    res.status(500).send({ error: err.message });
+  }
 });
 
 app.listen(process.env.PORT || 8080, () => console.log("Server running"));
